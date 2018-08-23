c26116d07bf2ef8dbfee50f72bfd6c6404e24100
edreeseg <reeseg@ubhcorg.umdnj.edu>
2018-08-23 09:32:51 -0400

    Chapter 3 exercises

---
+++ Chapter 3.js
@@ -0,0 +1,28 @@
+// Minimum
+
+const mathMin = (x, y) => {
+	if (arguments.length === 0)
+		return Infinity;
+	else if (typeof x !== "number" || typeof y !== "number")
+		return NaN;
+	else
+		x <= y ? x : y;
+	}
+	
+// Recursion
+
+const isEven = (n) => {
+	if (n === 0) return "Even";
+	else if (n === 1 || n === -1) return "Odd";
+	else return n > 0 ? isEven(n - 2) : isEven(n + 2);
+}
+
+// Bean Counting
+
+const countBs = (str) => str.match(/B/g) ? str.match(/B/g).length : 0;
+
+const countChar = (str, char) => str.match(RegExp(char, "g")) ? 
+	str.match(RegExp(char, "g")).length 
+	: 0;
+
+const countBs = (str) => countChar(str, "B");

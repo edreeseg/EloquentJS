bac86114bce117faf2b7dc2eb3eb917f7db75761
edreeseg <reeseg@ubhcorg.umdnj.edu>
2018-09-10 16:19:41 -0400

    Ch11 comments

--- Chapter 11 Code - Commented.js
+++ Chapter 11 Code - Commented.js
@@ -59,18 +59,59 @@ new Promise((_, reject) => reject(new Error("Fail")))
 		return "nothing";
 	})
 	.then(value => console.log("Handler 2", value));
+// -> Caught failure Error: Fail
+// -> Handler 2 nothing
 
 
 
+class Timeout extends Error {}
+
+function request(nest, target, type, content){
+	return new Promise((resolve, reject) => {
+		let done = false;
+		function attempt(n){
+			nest.send(target, type, content, (failed, value) => {
+				done = true;
+				if(failed) reject(failed);
+				else resolve(value);
+			});
+			setTimeout(() => {
+				if(done) return;
+				else if (n < 3) attempt(n + 1);
+				else reject(new Timeout('Timed out'));
+			}, 250);
+		}
+		attempt(1);
+	});
+}
+// Tries to send every quarter-second, and returns Timeout error if it fails thrice.
 
 
 
+function requestType(name, handler){
+	defineRequestType(name, (nest, content, source, callback) => {
+		try {
+			Promise.resolve(handler, (nest, content, source))
+				.then(response => callback(null, response),
+				failure => callback(failure));
+		} catch(exception){
+			callback(exception);
+		}
+	});
+}
+// ^^Wrapper for defineRequestType.  Allows handler to return a promise or plain value and wires that up to the callback.^^
 
+requestType('ping', () => 'pong');
 
-
-
-
-
+function availableNeighbors(nest){
+	let requests = nest.neighbors.map(neighbor => {
+		return request(nest, neighbor, 'ping')
+			.then(() => true, () => false);
+	});
+	return Promise.all(requests).then(result => {
+		return nest.neighbors.filter((_, i) => result[i]);
+	});
+}
 
 
 
